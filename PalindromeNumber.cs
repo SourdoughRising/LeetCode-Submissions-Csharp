a5a3c095f06d9d9025b9ab3b7fb6d4ac7d1947f8
Andrew Straub <freelancerzena@gmail.com>
2022-11-11 21:56:38 -0900

    Palindrome Number
    C# solution finding whether a number is identical if reversed

---
+++ PalindromeNumber.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        string input = Convert.ToString(x);
+        Console.WriteLine(input);
+        char[] charray = new char[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            charray[i] = input[i];
+        }
+        Console.WriteLine(charray);
+        string reverse = "";
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reverse += charray[i];
+        }
+        Console.WriteLine(reverse);
+
+        if (input == reverse)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
