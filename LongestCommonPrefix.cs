fe7086b0c6529c42669462a5f3cdc47eedaed920
Andrew Straub <freelancerzena@gmail.com>
2022-11-11 22:07:17 -0900

    Longest Common Prefix
    C# solution to finding the common prefix of any given array of strings

---
+++ LongestCommonPrefix.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        //find the shortest string
+
+
+        if (strs == null || strs.Length == 0) { return ""; Console.WriteLine("empty string returned"); }
+        for (int i = 0; i < strs[0].Length; i++)
+        {
+            char c = strs[0][i];
+            Console.WriteLine("c = {0}", c);
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (i == strs[j].Length || strs[j][i] != c)
+                {
+                    Console.WriteLine("strs[0].Substring(0,{0}) = {1}", i, strs[0].Substring(0, i));
+                    return strs[0].Substring(0, i);
+                }
+            }
+        }
+
+        return strs[0];
+    }
+}
