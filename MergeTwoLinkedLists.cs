60d45ad68c1db7aa14db1873a7820493d27f86e3
Andrew Straub <freelancerzena@gmail.com>
2022-11-15 21:07:24 -0900

    Merging two Linked Lists
    Merging two linked lists based in ascending order

---
+++ MergeTwoLinkedLists.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+    {
+
+        if (l1 == null) { return l2; }
+        if (l2 == null) { return l1; }
+
+        ListNode head;
+
+        if (l1.val < l2.val)
+        {
+            head = l1;
+            l1 = l1.next;
+        }
+        else
+        {
+            head = l2;
+            l2 = l2.next;
+        }
+
+        head.next = MergeTwoLists(l1, l2);
+
+        return head;
+    }
+}
